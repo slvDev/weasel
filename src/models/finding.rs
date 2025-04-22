4a974be5c25e476409219c98fcca73a84b79c8eb
slvDev <slava.serbov@gmail.com>
2025-04-22 10:45:57 +0300

    minor syntax fixes

--- src/models/finding.rs
+++ src/models/finding.rs
@@ -17,8 +17,8 @@ pub struct Finding {
     pub title: String,
     pub description: String,
     pub gas_savings: Option<usize>,
-    pub locations: Vec<Location>,
     pub example: Option<String>,
+    pub locations: Vec<Location>,
 }
 
 impl Finding {
@@ -28,22 +28,8 @@ impl Finding {
             title: title.to_string(),
             description: description.to_string(),
             gas_savings: None,
-            locations: Vec::new(),
             example: None,
+            locations: Vec::new(),
         }
-    }
-
-    pub fn add_location(&mut self, location: Location) {
-        self.locations.push(location);
-    }
-
-    pub fn with_gas_savings(mut self, savings: usize) -> Self {
-        self.gas_savings = Some(savings);
-        self
-    }
-
-    pub fn with_example(mut self, example: &str) -> Self {
-        self.example = Some(example.to_string());
-        self
     }
 }
